30e34c3e61a56d54b3978eac5b3c01a6ac1b24ab
Onur Haktan <onurhaktan243@gmail.com>
2023-06-23 16:33:14 +0300

    added detail page

--- ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/common/product.ts
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,5 +1,6 @@
 export class Product {
-    constructor(public sku:String,
+    constructor(public id: number,
+                public sku:String,
                 public name:String,
                 public description:String,
                 public unitPrice:number,
--- ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
- 
+  
   private baseUrl = 'http://localhost:8080/api/products';
   
   private categoryUrl = 'http://localhost:8080/api/product-category';
@@ -19,14 +19,12 @@ export class ProductService {
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-
     return this.getProducts(searchUrl);
   }
 
   searchProducts(theKeyword: string): Observable<Product[]>{
      // need to build URL based on keyword
      const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
-
      return this.getProducts(searchUrl);
   }
 
@@ -41,6 +39,12 @@ export class ProductService {
       map(response => response._embedded.productCategory)
     );
   }
+
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
+
 }
 
 interface GetResponseProducts {
