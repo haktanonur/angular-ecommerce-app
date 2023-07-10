1904adb6b038c05bcb73e056ae3f7d39e4949580
Onur Haktan <onurhaktan243@gmail.com>
2023-07-10 15:58:32 +0300

    added checkout form layout

---
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent {
+
+  checkoutFormGroup!: FormGroup; 
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+
+      }),
+      shippingAddress: this.formBuilder.group({
+        country: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        country: [''],
+        address: [''],
+        city: [''],
+        state: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+  }
+
+  onSubmit() { 
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+    const billingAddressControl = this.checkoutFormGroup.get('billingAddress');
+    const shippingAddressControl = this.checkoutFormGroup.get('shippingAddress');
+  
+    if (billingAddressControl && shippingAddressControl) {
+      if ((event.target as HTMLInputElement).checked) {
+        billingAddressControl.setValue(shippingAddressControl.value);
+      } else {
+        billingAddressControl.reset();
+      }
+    }
+  }
+  
+  
+
+}
