0d52a3d17889fc712fda31493d331b5a36c08ccb
Onur Haktan <onurhaktan243@gmail.com>
2023-07-11 17:11:12 +0300

    updated checkout form

--- ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -8,12 +9,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent {
 
-  checkoutFormGroup!: FormGroup; 
+  checkoutFormGroup!: FormGroup;
 
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {}
+  creditCartYears: number[] = [];
+  creditCartMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private luv2ShopFormService: Luv2ShopFormService) { }
 
   ngOnInit(): void {
 
@@ -47,17 +52,32 @@ export class CheckoutComponent {
         expirationYear: ['']
       })
     });
-  }
 
-  onSubmit() { 
-    console.log("Handling the submit button");
-    console.log(this.checkoutFormGroup.get('customer')?.value);
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("Start Month : " + startMonth);
+
+    // populate credit card months
+    this.luv2ShopFormService.getCreditCartMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCartMonths = data;
+      }
+    );
+
+    // populate credit card years
+    this.luv2ShopFormService.getCreditCartYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCartYears = data;
+      }
+    )
+
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
     const billingAddressControl = this.checkoutFormGroup.get('billingAddress');
     const shippingAddressControl = this.checkoutFormGroup.get('shippingAddress');
-  
+
     if (billingAddressControl && shippingAddressControl) {
       if ((event.target as HTMLInputElement).checked) {
         billingAddressControl.setValue(shippingAddressControl.value);
@@ -66,7 +86,36 @@ export class CheckoutComponent {
       }
     }
   }
-  
-  
 
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  handleMonthsAndYears(){
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.luv2ShopFormService.getCreditCartMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit cart months: " + JSON.stringify(data));
+        this.creditCartMonths = data;
+      }
+    );
+
+  }
 }
---
+++ ecommerce-project-2.0/02-backend/spring-boot-ecommerce/src/main/java/com/onurhaktan/ecommerce/entity/State.java
@@ -0,0 +1,25 @@
+package com.onurhaktan.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "state")
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+}
