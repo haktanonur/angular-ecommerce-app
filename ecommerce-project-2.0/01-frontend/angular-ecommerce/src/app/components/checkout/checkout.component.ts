0fab08cb742215d9d33d8522e5cba5ce4d07e019
Onur Haktan <onurhaktan243@gmail.com>
2023-07-18 16:02:02 +0300

    updated review cart

--- ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
@@ -22,9 +23,12 @@ export class CheckoutComponent {
   billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private luv2ShopFormService: Luv2ShopFormService) { }
+              private luv2ShopFormService: Luv2ShopFormService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -84,6 +88,19 @@ export class CheckoutComponent {
         this.countries = data;
       }
     );
+  }
+  reviewCartDetails() {
+
+    // subscribe to cart service for total quantity
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    // subscribe to cart service for total price
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );  
+
   }
 
   get firstName(){ return this.checkoutFormGroup.get('customer.firstName'); }
--- ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project-2.0/01-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
   constructor() { }
 
@@ -21,7 +21,7 @@ export class CartService {
   
     if (this.cartItems.length > 0) {
       existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
-      alreadyExistsInCart = (existingCartItem != undefined); // Kontrolü ekleyin
+      alreadyExistsInCart = (existingCartItem != undefined);
     }
   
     if (alreadyExistsInCart) {
